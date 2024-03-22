f94ba6fb05aeabfc48e1b1e24a62a509f5555ce7
SOM ON SOUM <somon.soum9@gmail.com>
2024-03-22 13:55:45 +0700

    refactor code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,8 @@
-import { LangButton } from "@/components/LangButton";
+import { LangButton } from "@/components/LangButton/LangButton";
 import { getI18n, getScopedI18n } from "@/locales/server";
+import { LocaleParams } from "@/types";
 
-type Props = {
-  params: { locale: "km" | "en" };
-};
-
-export default async function Home({ params }: Props) {
-  const { locale } = params;
-
+export default async function Home({ params: { locale } }: LocaleParams) {
   const t = await getI18n();
   const scopedT = await getScopedI18n("home.hello");
   const aboutT = await getScopedI18n("about");
@@ -15,7 +10,6 @@ export default async function Home({ params }: Props) {
   return (
     <div>
       <p>{t("home.hello")}</p>
-
       <p>{t("home.hello.world")}</p>
       <p>{scopedT("world")}</p>
       <p>{aboutT("title")}</p>
